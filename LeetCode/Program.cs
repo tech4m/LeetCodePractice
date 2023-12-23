2baa99fd7e346abbd09c0ab884366818e64f876e
tech4m <amitleo81@gmail.com>
2023-12-23 07:32:02 +0530

    first commit

---
+++ LeetCode/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QuestionsLibrary;
+
+Problem933 obj = new Problem933();
+
+obj.Ping(1);
+obj.Ping(100);
+obj.Ping(3001);
+obj.Ping(3002);  
+Console.WriteLine("Hello, World!");
+
---
+++ QuestionsLibrary/Problem933.cs
@@ -0,0 +1,27 @@
+﻿namespace QuestionsLibrary
+{
+    public class Problem933
+    {
+        Queue<int> recentCallQueue = new Queue<int>();
+        public Problem933()
+        {
+            recentCallQueue.Enqueue(1);
+            recentCallQueue.Enqueue(100);
+            recentCallQueue.Enqueue(3001);
+            recentCallQueue.Enqueue(3002);
+        }
+
+        public int Ping(int t)
+        { 
+            int minTime = t - 3000; 
+
+            recentCallQueue.Enqueue(t);
+
+            while (recentCallQueue.Peek() < minTime)
+            {
+                recentCallQueue.Dequeue();
+            }
+            return recentCallQueue.Count();
+        }
+    }
+}
